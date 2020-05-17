67c932a6318250f84120a6671604bb8ea1501599
Rafael Figueiredo <rafaael.figueiredo@gmail.com>
2020-05-17 20:14:16 -0300

    Feature - Add Pokemon Images from pokeresbastion

--- app/src/components/molecules/productImage/index.js
+++ app/src/components/molecules/productImage/index.js
@@ -1,16 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import imageSample from "../../../assets/images/productImageSample.png";
+import loader from "../../../assets/images/loader.gif";
+import pokemonImageNotFound from "../../../assets/images/pokemonImageNotFound.png";
 
 const ProductImage = styled.div`
   text-align: center;
   margin-top: -50px;
+  img {
+    max-width: 100px;
+  }
 `;
 
 export default (props) => {
+  const [imageIsLoaded, setImageIsLoaded] = useState(false);
+  const [image, setImage] = useState(null);
+  const imageURL = `https://pokeres.bastionbot.org/images/pokemon/${props.imageID}.png`;
+
+  function isImage(src) {
+    return new Promise((resolve, reject) => {
+      let img = new Image();
+      img.onload = () => resolve(img.src);
+      img.onerror = () => reject();
+      img.src = src;
+    });
+  }
+
+  const isImageLoaded = async (imageURL) => {
+    try {
+      const result = await isImage(imageURL);
+      setImage(result);
+      setImageIsLoaded(true);
+    } catch (error) {
+      setImage(pokemonImageNotFound);
+      setImageIsLoaded(true);
+    }
+  };
+
+  useEffect(() => {
+    isImageLoaded(imageURL);
+  }, [imageURL]);
+
   return (
     <ProductImage className="productImage">
-      <img src={props.image} />
+      {!imageIsLoaded && <img src={loader} />}
+      {imageIsLoaded && <img src={image} />}
     </ProductImage>
   );
 };
