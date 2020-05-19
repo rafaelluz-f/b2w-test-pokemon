22e243b80479e859fda69170ebc38ebe1fbca414
Rafael Figueiredo <rafaael.figueiredo@gmail.com>
2020-05-19 03:55:16 -0300

    Chore - Add global vars (react, reactdom, shallow, render and mount) inside setupTests.js

--- app/src/setupTests.js
+++ app/src/setupTests.js
@@ -2,4 +2,19 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import "@testing-library/jest-dom/extend-expect";
+import React from "react";
+import ReactDOM from "react-dom";
+import Enzyme, { shallow, render, mount } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import { createSerializer } from "enzyme-to-json";
+
+expect.addSnapshotSerializer(createSerializer({ mode: "deep" }));
+
+Enzyme.configure({ adapter: new Adapter() });
+
+global.React = React;
+global.ReactDOM = ReactDOM;
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
