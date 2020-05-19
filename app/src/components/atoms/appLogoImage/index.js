094885dadbf28d41f11ac302b9fcbffcd6a23caa
Rafael Figueiredo <rafaael.figueiredo@gmail.com>
2020-05-19 03:57:24 -0300

    Test - Add appLogoImage Test

--- app/src/components/atoms/appLogoImage/index.js
+++ app/src/components/atoms/appLogoImage/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
-const AppLogo = styled.img`
+const AppLogoImage = styled.img`
   margin-top: 20px;
   width: 100%;
   max-width: 500px;
 `;
 export default (props) => {
-  return <AppLogo alt={props.alt} src={props.src} />;
+  return <AppLogoImage alt={props.alt} src={props.src} />;
 };
