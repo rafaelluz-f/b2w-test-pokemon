b4b866b0781cb0c5c2852847d92fa89a7992ef4e
Rafael Figueiredo <rafaael.figueiredo@gmail.com>
2020-05-19 00:08:16 -0300

    Chore - refactoring cartButton molecule component

---
+++ app/src/components/atoms/cartButtonDisabled/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const CartButtonDisabled = styled.button`
+  color: #fff;
+  font-weight: 900;
+  text-transform: uppercase;
+  font-size: 32px;
+  padding: 25px 20px;
+  width: 100%;
+  border: none;
+  background: #a22525;
+  cursor: default;
+`;
+export default (props) => {
+  return (
+    <CartButtonDisabled className="disabled" onClick={props.function}>
+      {props.children}
+    </CartButtonDisabled>
+  );
+};
---
+++ app/src/components/atoms/cartButtonEnabled/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import styled from "styled-components";
+
+const CartButtonEnabled = styled.button`
+  background: green;
+  color: #fff;
+  font-weight: 900;
+  text-transform: uppercase;
+  font-size: 32px;
+  padding: 25px 20px;
+  width: 100%;
+  border: none;
+  cursor: pointer;
+`;
+export default (props) => {
+  return (
+    <CartButtonEnabled className="disabled" onClick={props.function}>
+      {props.children}
+    </CartButtonEnabled>
+  );
+};
